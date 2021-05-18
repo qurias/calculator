e0a20899bf65bcb47b304a56f2789ad1062dc77a
Ivan M <vania.morskov@yandex.ru>
2021-05-18 14:44:07 +0300

    redisign js - add new version of function random color and display counter, little fix in sass, renaming divs to buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let log = console.log;
-
-let math_symbols = document.querySelectorAll('.math_symbol');
-
+const log = console.log;
+const math_symbols = document.querySelectorAll('.math_symbol');
+const numeral = document.querySelector('.display');
+const buttons_numeral = document.querySelectorAll('.calc_btn');
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 math_symbols.forEach(e => {
@@ -11,68 +11,21 @@ math_symbols.forEach(e => {
 function changes_color() {
   let hexColor = '#';
   for (let i = 0; i < 6; i++) {
-      let randomNumber = Math.floor(Math.random() * hex.length);
-      hexColor += hex[randomNumber];
+    let randomNumber = Math.floor(Math.random() * hex.length);
+    hexColor += hex[randomNumber];
   };
   return hexColor;
 };
 
-let numeral = document.querySelector('.numeral');
-let equally = document.querySelector('.equally');
-let buttons_numeral = document.querySelectorAll('.button_numeral');
-
-var value1 = '';
-let value1_condition = true;
-let value2 = '';
-let value2_condition = false;
-let test = document.querySelector('.test');
-let math_symbol = '';
-
-
-math_symbols.forEach((symbol) => {
+buttons_numeral.forEach(symbol => {
+  let current = symbol.getAttribute('value');
   symbol.addEventListener('click', () => {
-    math_symbol += symbol.textContent
-    if (math_symbol === 'AC') {
-      value1 = ''
-      value2 = ''
-      log(value1)
-      log(value2)
+    if (current === 'c') {
+      numeral.innerHTML = '';
+    } else if (current === '=') {
+      numeral.innerHTML = eval(numeral.innerHTML);
+    } else {
+      numeral.innerHTML += current;
     }
-    if (math_symbol != '') {
-      value1_condition = false
-      value2_condition = true
-    }
-    numeral.innerHTML = ''
-  })
-})
-
-buttons_numeral.forEach( (button) => {
-  button.addEventListener('click', () => {
-    if(value1_condition) {
-      value1 += button.textContent
-      numeral.innerHTML = value1
-    }
-    else if (value2_condition) {
-      value2 += button.textContent
-      numeral.innerHTML = value2
-    }
-  })
-})
-
-equally.addEventListener('click', () => {
-  numeral.innerHTML = ''
-  let a = parseInt(value1,10)
-  let b = parseInt(value2,10)
-  if (math_symbol == '+') {
-    numeral.innerHTML = a + b
-  }
-  else if (math_symbol == '×') {
-    numeral.innerHTML = a * b
-  }
-  else if (math_symbol == '÷') {
-    numeral.innerHTML = a / b
-  }
-  else if (math_symbol == '-') {
-    numeral.innerHTML = a - b
-  }
-})
+  });
+});
